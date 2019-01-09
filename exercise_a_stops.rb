acb56c3acaa88571974fbf35a81baf99d84010a6
ianbone1 <ianbone1@googlemail.com>
2019-01-09 19:20:56 +0000

    Homework for Week 1, Day 3. Arrays, Hashes and loops

---
+++ exercise_a_stops.rb
@@ -0,0 +1,51 @@
+stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+
+# 1. Add `"Edinburgh Waverley"` to the end of the array
+
+#stops.push("Edinburgh Waverley")
+#puts stops
+
+# 2. Add `"Glasgow Queen St"` to the start of the array
+
+#stops.unshift("Glasgow Queen St")
+#puts stops
+
+# 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
+
+#stops.insert(3,"Polmont")
+#puts stops
+
+# 4. Work out the index position of `"Linlithgow"`
+
+#puts stops.index("Linlithgow")
+
+
+# 5. Remove `"Livingston"` from the array using its name
+
+#stops.delete("Livingston")
+#puts stops
+
+# 6. Delete `"Cumbernauld"` from the array by index
+
+# stops.delete_at(1)
+# puts stops
+
+# 7. How many stops there are in the array?
+
+#puts stops.length()
+
+# 8. How many ways can we return `"Falkirk High"` from the array?
+
+#puts stops[2]
+#puts stops.at(2)
+#puts stops[-4]
+
+# 9. Reverse the positions of the stops in the array
+
+#puts stops.reverse()
+
+# 10. Print out all the stops using a for loop
+
+#for stop in stops
+#  puts stop
+#end
