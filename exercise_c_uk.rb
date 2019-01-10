31b58af0d41aa059eb02b762581d09b2ebc9906e
ianbone1 <ianbone1@googlemail.com>
2019-01-10 08:48:40 +0000

    Answer 3 enhanced to use functions

--- exercise_c_uk.rb
+++ exercise_c_uk.rb
@@ -18,22 +18,39 @@ united_kingdom = [
 
 # 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
 
-#using known index of Swansea
+# v1: using known index of Swansea
 # united_kingdom[1][:capital]="Cardiff"
 # puts united_kingdom[1]
 
-#find Swansea index and then replace
-old_capital="Swansea"
-new_capital="Cardiff"
-index_counter=united_kingdom.length-1
-while index_counter>=0
-  if united_kingdom[index_counter][:capital]==old_capital
-    united_kingdom[index_counter][:capital]=new_capital
-  end
-  index_counter -= 1
-end
+# v2: find Swansea index and then replace
+# old_capital="Swansea"
+# new_capital="Cardiff"
+# index_counter=united_kingdom.length-1
+# while index_counter>=0
+#   if united_kingdom[index_counter][:capital]==old_capital
+#     united_kingdom[index_counter][:capital]=new_capital
+#   end
+#   index_counter -= 1
+# end
+#
+# puts united_kingdom
 
-puts united_kingdom
+# v3: use a methods
+# 
+# def change_capital(countries, change_capital_from, change_capital_to)
+#   index_counter=countries.length-1
+#   while index_counter>=0
+#     if countries[index_counter][:capital]==change_capital_from
+#       countries[index_counter][:capital]=change_capital_to
+#     end
+#     index_counter -= 1
+#   end
+# end
+#
+# old_capital="Swansea"
+# new_capital="Cardiff"
+# change_capital(united_kingdom, old_capital, new_capital)
+# puts united_kingdom
 
 # 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
 
