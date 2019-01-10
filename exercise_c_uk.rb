3c25efafe75123102ec510886a57834dfe9a7b93
ianbone1 <ianbone1@googlemail.com>
2019-01-10 08:38:50 +0000

    Enhanced q1 and 2 beyond MVP

--- exercise_c_uk.rb
+++ exercise_c_uk.rb
@@ -18,22 +18,40 @@ united_kingdom = [
 
 # 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
 
+#using known index of Swansea
 # united_kingdom[1][:capital]="Cardiff"
 # puts united_kingdom[1]
 
+#find Swansea index and then replace
+old_capital="Swansea"
+new_capital="Cardiff"
+index_counter=united_kingdom.length-1
+while index_counter>=0
+  if united_kingdom[index_counter][:capital]==old_capital
+    united_kingdom[index_counter][:capital]=new_capital
+  end
+  index_counter -= 1
+end
+
+puts united_kingdom
+
 # 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
 
-# nother_ireland={name: "Northern Ireland",
+# nother_ireland={name:       "Northern Ireland",
 #                 populations: 2,
-#               capital: "Belfast"}
+#                 capital:    "Belfast"}
 # united_kingdom.push(nother_ireland)
 # puts united_kingdom
 
 # 3. Use a loop to print the names of all the countries in the UK.
 
+# using  loop
 # for country in united_kingdom
 #   puts country[:name]
 # end
+
+# using .each
+# united_kingdom.each() {|country| puts country[:name]}
 
 # 4. Use a loop to find the total population of the UK.
 
